318f7a1f96e6994ff1d2ca4b7a2005ce81d13c3e
Daniil <danya.moiseev@inbox.ru>
2022-06-30 13:23:52 +1000

    back-a-client

---
+++ server/controllers/basketController.js
@@ -0,0 +1,8 @@
+const apiError = require('../error/apiError')
+const {Basket} = require('../models/models')
+
+class Basket {
+    async addDevice(req, res) {
+        
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const router = new Router()
 const deviceRouter = require('./deviceRoute')
 const userRouter = require('./userRoute')
 const typeRouter = require('./typeRoute')
+const basketRoute = require('./basketRoute')
 
 router.use('/user', userRouter)
 router.use('/device', deviceRouter)
 router.use('/type', typeRouter)
+router.use('/basket', basketRoute)
 
 module.exports = router
